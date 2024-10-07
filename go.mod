8bdc37ee467ae171c0cda7d6ae9522781c4f3b0e
Nicolás Salvo <nicosalvo@gmail.com>
2024-10-07 21:27:50 +0000

    feat: add initial server and Templ

--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
-module unit-converter
+module github.com/ngsalvo/roadmapsh-unit-converter
 
 go 1.23.0
 
 require github.com/stretchr/testify v1.9.0
 
 require (
+	github.com/a-h/templ v0.2.778 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"log/slog"
+	"net/http"
+	"os"
 
+	"github.com/ngsalvo/roadmapsh-unit-converter/components"
+)
+
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	router := http.NewServeMux()
+	server := http.Server{Addr: ":3000", Handler: router}
+
+	router.HandleFunc("/", helloWorldHandler)
+
+	logger.Info("Starting server on port 3000")
+	server.ListenAndServe()
+
+}
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	components.Home("ME!").Render(r.Context(), w)
 }
