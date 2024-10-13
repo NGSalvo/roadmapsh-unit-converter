9f1af9ac67ffcad8b505f1aa33083183a04fec7f
Nicolás Salvo <nicosalvo@gmail.com>
2024-10-13 01:39:18 +0000

    feat: add proxy for windows reload

--- dev.sh
+++ dev.sh
@@ -5,6 +5,6 @@ npx tailwindcss \
  -i 'static/styles/main.css' \
  -o 'static/styles/style.css' \
  --watch & \
- templ generate -watch
+ templ generate -watch -proxy="http://localhost:3000"
 
  # templ generate -watch -proxy="http://localhost:3000" -cmd="go run main.go"
