2f3f705c2f2a13b766443034c4881efe34587abe
Nicolás Salvo <nicosalvo@gmail.com>
2024-10-30 20:25:36 +0000

    refactor: remove comments

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/delaneyj/datastar"
 	"github.com/go-chi/chi"
@@ -14,59 +13,9 @@ import (
 	"github.com/ngsalvo/roadmapsh-unit-converter/services"
 )
 
-type Middleware func(http.Handler) http.Handler
-
-func CreateMiddlewareStack(m ...Middleware) Middleware {
-	return func(h http.Handler) http.Handler {
-		for i := len(m) - 1; i >= 0; i-- {
-			h = m[i](h)
-		}
-		return h
-	}
-}
-
-type wrappedWriter struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (w *wrappedWriter) WriteHeader(statusCode int) {
-	w.ResponseWriter.WriteHeader(statusCode)
-	w.statusCode = statusCode
-}
-
-// should be in its own package
-func LoggingMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		wrapped := &wrappedWriter{w, http.StatusOK}
-
-		next.ServeHTTP(wrapped, r)
-		log.Printf("%d %s %s %s", wrapped.statusCode, r.Method, r.URL.Path, time.Since(start))
-	})
-}
-
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
-	//router := http.NewServeMux()
-
-	//middlewares := CreateMiddlewareStack(
-	//LoggingMiddleware,
-	//)
-
-	//router.HandleFunc("GET /", homeHandler)
-	//router.Handle("GET /static/", http.StripPrefix("/static/", fileServer))
-	//router.HandleFunc("POST /result", resultHandler)
-	//router.HandleFunc("GET /tabs/update", getTab)
-
-	// nesting path
-	//v1 := http.NewServeMux()
-	//v1.Handle("/v1/", http.StripPrefix("/v1/", router))
-
-	//server := http.Server{Addr: ":3000", Handler: middlewares(router)}
-
-	//server.ListenAndServe()
 
 	router := chi.NewRouter()
 
